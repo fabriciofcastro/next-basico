556a3eaaf8a731caf5f0aef0b9692b9b41032df5
Fabricio Castro <fabriciofeitosacastro@gmail.com>
2022-07-11 16:19:26 -0300

    Introdução API nextjs

--- src/pages/api/_api1.js
+++ src/pages/api/_api1.js
@@ -1,5 +1,9 @@
 export default (req, res) => {
+    if(req.method === "GET") {
+        res.status(200).json({ nome: "Pedro"})
+    }
+
     res.status(200).json({
         metodo: req.method
     })
-}
+}   
